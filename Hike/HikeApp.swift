ec672daab201a5d6a2161aa51a284c9e4e4c977c
htnguy <hieu.nguyen4e@gmail.com>
2024-06-01 19:21:36 -0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Hieu Nguyen on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
